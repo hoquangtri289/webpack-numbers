0c4a013a62775e7498381bb5288a440629929ed0
Ho Quang Tri <hoquangtri289@gmail.com>
2021-09-26 10:09:59 +0700

    ...

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+import path from 'path';
+
+export default {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'webpack-number.js',
+        library: "webpackNumbers"
+    }
+}
